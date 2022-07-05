21ce966f0bab16f92c1513bf4903df65c861e66d
laelatuzzahro <laelatuzzahro88@gmail.com>
2022-07-05 20:52:39 +0700

    fixing connection

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,9 @@
 <?php
-	$konek = new mysqli('localhost','root','','pw');
+	$hostname='localhost';
+	$username='root';
+	$password='';
+	$database='pw';
+	$konek = new mysqli($hostname,$username,$password,$database);
 	if($konek->connect_error){
 		die ('Maaf koneksi gagal:' .$connection->connect_error);}
 ?>
